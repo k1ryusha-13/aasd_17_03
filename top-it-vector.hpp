fa8b2883c61390835c6849793b946c477b7093f2
kirusha-13 <kirillukashevich2007@gmail.com>
2026-03-17 11:34:11 +0300

    Обьявление методов, и задачи на нектт занятие

--- top-it-vector.hpp
+++ top-it-vector.hpp
@@ -8,27 +8,43 @@ namespace topit
   struct Vector {
     Vector();
     ~Vector();
+    Vector(const Vector&);
+    Vector(Vector&&);
+    Vector& operator=(const Vector&);
+    Vector& operator=(Vector&&);
 
     bool isEmpty() const noexcept;
+    size_t getSize() const noexcept;//что нибудь из этого или все + протестировать
+    size_t getCapacity() const noexcept;
+
+    void pushBack(const T& v);//domashka
+    void popBack();//и этого к след аисду
+    void insert(size_t i, const T& v);
+    void erase(size_t i);
 
     private:
       T* data_;
       size_t size_, capacity_;
   };
 }
+template< class T >
+void topit::Vector< T >::pushBack(const T& v)
+{}
 
 template< class T >
 bool topit::Vector< T >::isEmpty() const noexcept
 {
-  return false;
+  return !size_;
 }
 template< class T >
 topit::Vector< T >::~Vector()
-{}
+{
+  delete[] data_;
+}
 template< class T >
 topit::Vector< T >::Vector():
-  data_(),
-  size_(),
-  capacity_()
+  data_(nullptr),
+  size_(0),
+  capacity_(0)
 {}
 #endif
